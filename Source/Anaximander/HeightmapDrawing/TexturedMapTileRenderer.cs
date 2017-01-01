655e1a8c09c79a7847240ed61948c1fc63a68474
Ricky Curtice <kf6kjg@users.noreply.github.com>
2017-01-01 12:14:34 -0800

    Reformatted and rebuilt the heightmap rendering.
    Now interpolates between the ocean and beach color based on water depth.  This provides a look much closer to SL's.
    
    Disabled the self-shadowing code as it was causing large blocks of terrain to go dark and look weird.
    
    Rebuilt the water-terrain logic to make things cleaner and be able to get underwater detail when the shadowing code gets fixed.

--- Source/Anaximander/HeightmapDrawing/TexturedMapTileRenderer.cs
+++ Source/Anaximander/HeightmapDrawing/TexturedMapTileRenderer.cs
@@ -67,16 +67,14 @@ namespace Anaximander {
 		public float s;
 		public float v;
 
-		public HSV(float h, float s, float v)
-		{
+		public HSV(float h, float s, float v) {
 			this.h = h;
 			this.s = s;
 			this.v = v;
 		}
 
 		// (for info about algorithm, see http://en.wikipedia.org/wiki/HSL_and_HSV)
-		public HSV(Color c)
-		{
+		public HSV(Color c) {
 			float r = c.R / 255f;
 			float g = c.G / 255f;
 			float b = c.B / 255f;
@@ -84,25 +82,35 @@ namespace Anaximander {
 			float min = Math.Min(Math.Min(r, g), b);
 			float diff = max - min;
 
-			if (Math.Abs(max - min) < 0.001) h = 0f;
-			else if (Math.Abs(max - r) < 0.001) h = (g - b) / diff * 60f;
-			else if (Math.Abs(max - g) < 0.001) h = (b - r) / diff * 60f + 120f;
-			else h = (r - g) / diff * 60f + 240f;
-			if (h < 0f) h += 360f;
+			if (Math.Abs(max - min) < 0.001)
+				h = 0f;
+			else if (Math.Abs(max - r) < 0.001)
+				h = (g - b) / diff * 60f;
+			else if (Math.Abs(max - g) < 0.001)
+				h = (b - r) / diff * 60f + 120f;
+			else
+				h = (r - g) / diff * 60f + 240f;
+			if (h < 0f)
+				h += 360f;
 
-			if (Math.Abs(max) <= 0.001f) s = 0f;
-			else s = diff / max;
+			if (Math.Abs(max) <= 0.001f)
+				s = 0f;
+			else
+				s = diff / max;
 
 			v = max;
 		}
 
 		// (for info about algorithm, see http://en.wikipedia.org/wiki/HSL_and_HSV)
-		public Color ToColor()
-		{
-			if (s < 0f) LOG.Debug("S < 0: " + s);
-			else if (s > 1f) LOG.Debug("S > 1: " + s);
-			if (v < 0f) LOG.Debug("V < 0: " + v);
-			else if (v > 1f) LOG.Debug("V > 1: " + v);
+		public Color ToColor() {
+			if (s < 0f)
+				LOG.Debug("S < 0: " + s);
+			else if (s > 1f)
+				LOG.Debug("S > 1: " + s);
+			if (v < 0f)
+				LOG.Debug("V < 0: " + v);
+			else if (v > 1f)
+				LOG.Debug("V > 1: " + v);
 
 			float f = h / 60f;
 			int sector = (int)f % 6;
@@ -112,29 +120,36 @@ namespace Anaximander {
 			int ti = (int)(v * (1f - (1f - f) * s) * 255f);
 			int vi = (int)(v * 255f);
 
-			if (pi < 0) pi = 0;
-			if (pi > 255) pi = 255;
-			if (qi < 0) qi = 0;
-			if (qi > 255) qi = 255;
-			if (ti < 0) ti = 0;
-			if (ti > 255) ti = 255;
-			if (vi < 0) vi = 0;
-			if (vi > 255) vi = 255;
+			if (pi < 0)
+				pi = 0;
+			if (pi > 255)
+				pi = 255;
+			if (qi < 0)
+				qi = 0;
+			if (qi > 255)
+				qi = 255;
+			if (ti < 0)
+				ti = 0;
+			if (ti > 255)
+				ti = 255;
+			if (vi < 0)
+				vi = 0;
+			if (vi > 255)
+				vi = 255;
 
-			switch (sector)
-			{
+			switch (sector) {
 				case 0:
-				return Color.FromArgb(vi, ti, pi);
+					return Color.FromArgb(vi, ti, pi);
 				case 1:
-				return Color.FromArgb(qi, vi, pi);
+					return Color.FromArgb(qi, vi, pi);
 				case 2:
-				return Color.FromArgb(pi, vi, ti);
+					return Color.FromArgb(pi, vi, ti);
 				case 3:
-				return Color.FromArgb(pi, qi, vi);
+					return Color.FromArgb(pi, qi, vi);
 				case 4:
-				return Color.FromArgb(ti, pi, vi);
+					return Color.FromArgb(ti, pi, vi);
 				default:
-				return Color.FromArgb(vi, pi, qi);
+					return Color.FromArgb(vi, pi, qi);
 			}
 		}
 	}
@@ -172,14 +187,18 @@ namespace Anaximander {
 
 		// interpolate two colors in HSV space and return the resulting color
 		private static HSV interpolateHSV(ref HSV c1, ref HSV c2, float ratio) {
-			if (ratio <= 0f) return c1;
-			if (ratio >= 1f) return c2;
+			if (ratio <= 0f)
+				return c1;
+			if (ratio >= 1f)
+				return c2;
 
 			// make sure we are on the same side on the hue-circle for interpolation
 			// We change the hue of the parameters here, but we don't change the color
 			// represented by that value
-			if (c1.h - c2.h > 180f) c1.h -= 360f;
-			else if (c2.h - c1.h > 180f) c1.h += 360f;
+			if (c1.h - c2.h > 180f)
+				c1.h -= 360f;
+			else if (c2.h - c1.h > 180f)
+				c1.h += 360f;
 
 			return new HSV(c1.h * (1f - ratio) + c2.h * ratio,
 				c1.s * (1f - ratio) + c2.s * ratio,
@@ -195,7 +214,7 @@ namespace Anaximander {
 		/// <param name="x">The x coordinate.</param>
 		/// <param name="y">The y coordinate.</param>
 		private static double getHeight(double[,] hm, double x, double y) {
-			int x_0 = (int)x,  y_0 = (int)y;
+			int x_0 = (int)x, y_0 = (int)y;
 			int x_1 = x_0 + 1, y_1 = y_0 + 1;
 
 			var x_ratio = x - x_0; // The fractional part gives the 0-1 ratio needed.
@@ -205,29 +224,25 @@ namespace Anaximander {
 			if (x_0 < hm.GetLength(0) - 1 && y_0 < hm.GetLength(1) - 1)
 				return
 					hm[x_0, y_0] * (1 - x_ratio) * (1 - y_ratio) +
-					hm[x_1, y_0] *      x_ratio  * (1 - y_ratio) +
-					hm[x_0, y_1] * (1 - x_ratio) *      y_ratio  +
-					hm[x_1, y_1] *      x_ratio  *      y_ratio
-					;
+				hm[x_1, y_0] * x_ratio * (1 - y_ratio) +
+				hm[x_0, y_1] * (1 - x_ratio) * y_ratio +
+				hm[x_1, y_1] * x_ratio * y_ratio;
 			else if (x_0 < hm.GetLength(0) - 1)
 				return
 					hm[x_0, y_0] * (1 - x_ratio) * (1 - y_ratio) +
-					hm[x_1, y_0] *      x_ratio  * (1 - y_ratio)
-					;
+				hm[x_1, y_0] * x_ratio * (1 - y_ratio);
 			else if (y_0 < hm.GetLength(1) - 1)
 				return
 					hm[x_0, y_0] * (1 - x_ratio) * (1 - y_ratio) +
-					hm[x_0, y_1] * (1 - x_ratio) *      y_ratio
-					;
+				hm[x_0, y_1] * (1 - x_ratio) * y_ratio;
 			else
 				return
-					hm[x_0, y_0]
-					;
+					hm[x_0, y_0];
 		}
+
 		#endregion
 
-		public static void TerrainToBitmap(DataReader.Region region, DirectBitmap mapbmp)
-		{
+		public static void TerrainToBitmap(DataReader.Region region, DirectBitmap mapbmp) {
 			var textures = new Texture[4];
 			textures[0] = new Texture(UUID.Parse(region.terrainTexture1), CommonTextures.TERRAIN_TEXTURE_1.AverageColor);
 			textures[1] = new Texture(UUID.Parse(region.terrainTexture2), CommonTextures.TERRAIN_TEXTURE_2.AverageColor);
@@ -257,115 +272,108 @@ namespace Anaximander {
 
 			var waterHeight = region.waterHeight;
 
-			for (int x = 0; x < mapbmp.Width; x++)
-			{
+			for (int x = 0; x < mapbmp.Width; x++) {
 				var columnRatio = (double)x / (mapbmp.Width - 1); // 0 - 1, for interpolation
 
-				for (int y = 0; y < mapbmp.Height; y++)
-				{
+				for (int y = 0; y < mapbmp.Height; y++) {
 					var rowRatio = (double)y / (mapbmp.Height - 1); // 0 - 1, for interpolation
 
 					// Y flip the cordinates for the bitmap: hf origin is lower left, bm origin is upper left
 					var yr = (mapbmp.Height - 1) - y;
 
 					var heightvalue = mapbmp.Width == 256 ?
-						getHeight(region.heightmapData, (int)(255 * columnRatio), (int)(255 * rowRatio)) :
+						getHeight(region.heightmapData, x, y) :
 						getHeight(region.heightmapData, 255 * columnRatio, 255 * rowRatio);
 
 					if (Double.IsInfinity(heightvalue) || Double.IsNaN(heightvalue))
 						heightvalue = 0d;
 
-					if (heightvalue > waterHeight)
-					{
-						// add a bit noise for breaking up those flat colors:
-						// - a large-scale noise, for the "patches" (using an doubled s-curve for sharper contrast)
-						// - a small-scale noise, for bringing in some small scale variation
-						//float bigNoise = (float)TerrainUtil.InterpolatedNoise(x / 8.0, y / 8.0) * .5f + .5f; // map to 0.0 - 1.0
-						//float smallNoise = (float)TerrainUtil.InterpolatedNoise(x + 33, y + 43) * .5f + .5f;
-						//float hmod = heightvalue + smallNoise * 3f + S(S(bigNoise)) * 10f;
-						var hmod =
-							heightvalue +
-							TerrainUtil.InterpolatedNoise(x + 33, y + 43) * 1.5f + 1.5f + // 0 - 3
-							Scurve(Scurve(TerrainUtil.InterpolatedNoise(x / 8.0, y / 8.0) * .5f + .5f)) * 10f; // 0 - 10
+					var tileScalarX = 256f / mapbmp.Width; // Used to hack back in those constants that were hand-tuned to 256px tiles.
+					var tileScalarY = 256f / mapbmp.Height; // Used to hack back in those constants that were hand-tuned to 256px tiles.
 
-						// find the low/high values for this point (interpolated bilinearily)
-						// (and remember, x=0,y=0 is SW)
-						var low = levelSWlow * (1f - rowRatio) * (1f - columnRatio) +
-							levelSElow * (1f - rowRatio) * columnRatio +
-							levelNWlow * rowRatio * (1f - columnRatio) +
-							levelNElow * rowRatio * columnRatio;
-						var high = levelSWhigh * (1f - rowRatio) * (1f - columnRatio) +
-							levelSEhigh * (1f - rowRatio) * columnRatio +
-							levelNWhigh * rowRatio * (1f - columnRatio) +
-							levelNEhigh * rowRatio * columnRatio;
-						if (high < low)
-						{
-							// someone tried to fool us. High value should be higher than low every time
-							var tmp = high;
-							high = low;
-							low = tmp;
-						}
+					// add a bit noise for breaking up those flat colors:
+					// - a large-scale noise, for the "patches" (using an doubled s-curve for sharper contrast)
+					// - a small-scale noise, for bringing in some small scale variation
+					//float bigNoise = (float)TerrainUtil.InterpolatedNoise(x / 8.0, y / 8.0) * .5f + .5f; // map to 0.0 - 1.0
+					//float smallNoise = (float)TerrainUtil.InterpolatedNoise(x + 33, y + 43) * .5f + .5f;
+					//float hmod = heightvalue + smallNoise * 3f + S(S(bigNoise)) * 10f;
+					var hmod =
+						heightvalue +
+						TerrainUtil.InterpolatedNoise(tileScalarX * (x + 33 + (int)region.locationX * mapbmp.Width), tileScalarY * (y + 43 + (int)region.locationY * mapbmp.Height)) * 1.5f + 1.5f + // 0 - 3
+						Scurve(Scurve(TerrainUtil.InterpolatedNoise(tileScalarX * (x + (int)region.locationX * mapbmp.Width) / 8.0, tileScalarY * (y + (int)region.locationY * mapbmp.Height) / 8.0) * .5f + .5f)) * 10f; // 0 - 10
 
-						HSV hsv;
-						if (hmod <= low) hsv = hsv1; // too low
-						else if (hmod >= high) hsv = hsv4; // too high
-						else
-						{
+					// find the low/high values for this point (interpolated bilinearily)
+					// (and remember, x=0,y=0 is SW)
+					var low = levelSWlow * (1f - rowRatio) * (1f - columnRatio) +
+					           levelSElow * (1f - rowRatio) * columnRatio +
+					           levelNWlow * rowRatio * (1f - columnRatio) +
+					           levelNElow * rowRatio * columnRatio;
+					var high = levelSWhigh * (1f - rowRatio) * (1f - columnRatio) +
+					            levelSEhigh * (1f - rowRatio) * columnRatio +
+					            levelNWhigh * rowRatio * (1f - columnRatio) +
+					            levelNEhigh * rowRatio * columnRatio;
+					if (high < low) {
+						// someone tried to fool us. High value should be higher than low every time
+						var tmp = high;
+						high = low;
+						low = tmp;
+					}
+
+					HSV hsv;
+					if (heightvalue > waterHeight) {
+						// Above water
+						if (hmod <= low)
+							hsv = hsv1; // too low
+							else if (hmod >= high)
+							hsv = hsv4; // too high
+							else {
 							// HSV-interpolate along the colors
 							// first, rescale h to 0.0 - 1.0
 							hmod = (hmod - low) / (high - low);
 							// now we have to split: 0.00 => color1, 0.33 => color2, 0.67 => color3, 1.00 => color4
-							if (hmod < 1d/3d) hsv = interpolateHSV(ref hsv1, ref hsv2, (float)(hmod * 3d));
-							else if (hmod < 2d/3d) hsv = interpolateHSV(ref hsv2, ref hsv3, (float)((hmod * 3d) - 1d));
-							else hsv = interpolateHSV(ref hsv3, ref hsv4, (float)((hmod * 3d) - 2d));
+							if (hmod < 1d / 3d)
+								hsv = interpolateHSV(ref hsv1, ref hsv2, (float)(hmod * 3d));
+							else if (hmod < 2d / 3d)
+								hsv = interpolateHSV(ref hsv2, ref hsv3, (float)((hmod * 3d) - 1d));
+							else
+								hsv = interpolateHSV(ref hsv3, ref hsv4, (float)((hmod * 3d) - 2d));
 						}
-
-						// Shade the terrain for shadows
-						if (x < (mapbmp.Width - 1) && y < (mapbmp.Height - 1))
-						{
-							var hfvaluecompare = getHeight(region.heightmapData, (x + 1) / (mapbmp.Width - 1), (y + 1) / (mapbmp.Height - 1)); // light from north-east => look at land height there
-							if (Double.IsInfinity(hfvaluecompare) || Double.IsNaN(hfvaluecompare))
-								hfvaluecompare = 0d;
-
-							var hfdiff = heightvalue - hfvaluecompare;  // => positive if NE is lower, negative if here is lower
-							hfdiff *= 0.06d; // some random factor so "it looks good"
-							if (hfdiff > 0.02d)
-							{
-								var highlightfactor = 0.18d;
-								// NE is lower than here
-								// We have to desaturate and lighten the land at the same time
-								hsv.s = (hsv.s - (hfdiff * highlightfactor) > 0d) ? (float) (hsv.s - (hfdiff * highlightfactor)) : 0f;
-								hsv.v = (hsv.v + (hfdiff * highlightfactor) < 1d) ? (float) (hsv.v + (hfdiff * highlightfactor)) : 1f;
-							}
-							else if (hfdiff < -0.02f)
-							{
-								// here is lower than NE:
-								// We have to desaturate and blacken the land at the same time
-								hsv.s = (hsv.s + hfdiff > 0f) ? (float) (hsv.s + hfdiff) : 0f;
-								hsv.v = (hsv.v + hfdiff > 0f) ? (float) (hsv.v + hfdiff) : 0f;
-							}
-						}
-						mapbmp.Bitmap.SetPixel(x, yr, hsv.ToColor());
 					}
-					else
-					{
-						// We're under the water level with the terrain, so paint water instead of land
+					else {
+						// Under water.
+						var deepwater = new HSV(_waterColor);
+						var beachwater = new HSV(_beachColor);
 
-						heightvalue = waterHeight - heightvalue;
-						if (Double.IsInfinity(heightvalue) || Double.IsNaN(heightvalue))
-							heightvalue = 0f;
-						else if (heightvalue > 19f)
-							heightvalue = 19f;
-						else if (heightvalue < 0f)
-							heightvalue = 0f;
+						var water = interpolateHSV(ref deepwater, ref beachwater, (float)Scurve(heightvalue / waterHeight));
 
-						heightvalue = 100f - (heightvalue * 100f) / 19f;  // 0 - 19 => 100 - 0
-
-						var water = new HSV(_waterColor);
-						water.v = (float)heightvalue / 255f;
-
-						mapbmp.Bitmap.SetPixel(x, yr, water.ToColor());
+						hsv = water;
 					}
+
+					// Shade the terrain for shadows
+					//if (x < (mapbmp.Width - 1) && y < (mapbmp.Height - 1)) {
+					//	var hfvaluecompare = getHeight(region.heightmapData, (x + 1) / (mapbmp.Width - 1), (y + 1) / (mapbmp.Height - 1)); // light from north-east => look at land height there
+					//	if (Double.IsInfinity(hfvaluecompare) || Double.IsNaN(hfvaluecompare))
+					//		hfvaluecompare = 0d;
+					//
+					//	var hfdiff = heightvalue - hfvaluecompare;  // => positive if NE is lower, negative if here is lower
+					//	hfdiff *= 0.06d; // some random factor so "it looks good"
+					//	if (hfdiff > 0.02d) {
+					//		var highlightfactor = 0.18d;
+					//		// NE is lower than here
+					//		// We have to desaturate and lighten the land at the same time
+					//		hsv.s = (hsv.s - (hfdiff * highlightfactor) > 0d) ? (float)(hsv.s - (hfdiff * highlightfactor)) : 0f;
+					//		hsv.v = (hsv.v + (hfdiff * highlightfactor) < 1d) ? (float)(hsv.v + (hfdiff * highlightfactor)) : 1f;
+					//	}
+					//	else if (hfdiff < -0.02f) {
+					//		var highlightfactor = 1.0d;
+					//		// here is lower than NE:
+					//		// We have to desaturate and blacken the land at the same time
+					//		hsv.s = (hsv.s + (hfdiff * highlightfactor) > 0f) ? (float)(hsv.s + (hfdiff * highlightfactor)) : 0f;
+					//		hsv.v = (hsv.v + (hfdiff * highlightfactor) > 0f) ? (float)(hsv.v + (hfdiff * highlightfactor)) : 0f;
+					//	}
+					//}
+
+					mapbmp.Bitmap.SetPixel(x, yr, hsv.ToColor());
 				}
 			}
 			LOG.Info("[TERRAIN]: Done in " + (Environment.TickCount - tc) + " ms");
