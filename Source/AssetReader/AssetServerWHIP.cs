13f599a854d02edbd31fe1c9c6fca868733c1ca9
Ricky Curtice <kf6kjg@users.noreply.github.com>
2017-02-26 20:14:44 -0800

    Make WHIP use the good logging ideas that came from implementing CF.

--- Source/AssetReader/AssetServerWHIP.cs
+++ Source/AssetReader/AssetServerWHIP.cs
@@ -41,30 +41,34 @@ namespace AssetReader {
 		public int Port { get; private set; }
 		public string Password { get; private set; }
 
-		private RemoteServer _whipServer = null;
+		private string _configSectionName { get; set; }
+
+		private RemoteServer _provider = null;
 
 		public void Dispose() {
 			try {
-				_whipServer?.Stop();
+				_provider?.Stop();
 			}
 #pragma warning disable RECS0022 // A catch clause that catches System.Exception and has an empty body
 			catch {
 				// Nothing to do here.
 			}
 #pragma warning restore RECS0022 // A catch clause that catches System.Exception and has an empty body
-			_whipServer = null;
+			_provider = null;
 		}
 
 		public void Initialize(IConfig settings) {
+			_configSectionName = settings.Name;
+
 			Host = settings.GetString("Host", string.Empty);
 			Port = settings.GetInt("Port", 32700);
 			Password = settings.GetString("Password", "changeme"); // Yes, that's the default password for WHIP.
 
-			_whipServer = new RemoteServer(Host, (ushort) Port, Password);
-			_whipServer.Start(); // TODO: this needs to be started when needed, and shutdown after no usage for a period of time.  Noted that the library doesn't like repeated start stops, it seems to not keep up the auth info, so the whole _whipServer instance would need to be scrapped and reinitialized.
-			var status = _whipServer.GetServerStatus();
+			_provider = new RemoteServer(Host, (ushort) Port, Password);
+			_provider.Start(); // TODO: this needs to be started when needed, and shutdown after no usage for a period of time.  Noted that the library doesn't like repeated start stops, it seems to not keep up the auth info, so the whole _whipServer instance would need to be scrapped and reinitialized.
+			var status = _provider.GetServerStatus();
 
-			LOG.Info($"[WHIP_SERVER] WHIP connection prepared for host {Host}:{Port}\n'{status}'.");
+			LOG.Info($"[WHIP_SERVER] [{_configSectionName}] WHIP connection prepared for host {Host}:{Port}\n'{status}'.");
 		}
 
 		public async Task<AssetBase> RequestAssetAsync(UUID assetID) {
@@ -75,14 +79,14 @@ namespace AssetReader {
 			Asset asset = null;
 
 			try {
-				asset = _whipServer.GetAsset(assetID.ToString());
+				asset = _provider.GetAsset(assetID.ToString());
 			}
 			catch (AssetServerError e) {
-				LOG.Error($"[WHIP_SERVER] Error getting asset from server at ", e);
+				LOG.Error($"[WHIP_SERVER] [{_configSectionName}] Error getting asset from server.", e);
 				return null;
 			}
 			catch (AuthException e) {
-				LOG.Error($"[WHIP_SERVER] Authentication error getting asset from server.", e);
+				LOG.Error($"[WHIP_SERVER] [{_configSectionName}] Authentication error getting asset from server.", e);
 				return null;
 			}
 
